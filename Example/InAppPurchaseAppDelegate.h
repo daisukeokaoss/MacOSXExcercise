51a554c065e0c2f8fb03c5a7f9b94c8b62137823
Daisuke Oka <daisuke_oka@nanosoftware.biz>
2014-08-15 19:11:19 +0900

    Initial Commit

---
+++ Example/InAppPurchaseAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  InAppPurchaseAppDelegate.h
+//  Example
+//
+//  Created by おかやん on 2014/08/15.
+//  Copyright (c) 2014年 ナノソフトウェア. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface InAppPurchaseAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Example/InAppPurchaseAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  InAppPurchaseAppDelegate.m
+//  Example
+//
+//  Created by おかやん on 2014/08/15.
+//  Copyright (c) 2014年 ナノソフトウェア. All rights reserved.
+//
+
+#import "InAppPurchaseAppDelegate.h"
+
+@implementation InAppPurchaseAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
